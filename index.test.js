f0b0ccb22613dcfa1abf3d57c603dcaf29e14662
Wilfred Hughes <me@wilfred.me.uk>
2019-07-09 23:26:05 +0100

    Basic test suite

---
+++ index.test.js
@@ -0,0 +1,24 @@
+const commonmark = require("commonmark");
+const transform = require("./index");
+
+function transformAndRender(src) {
+  const reader = new commonmark.Parser();
+  const writer = new commonmark.HtmlRenderer();
+  const parsed = reader.parse(src);
+
+  return writer.render(transform(parsed)).trim();
+}
+
+test("Text without WikiWords", () => {
+  expect(transformAndRender("foo")).toBe("<p>foo</p>");
+});
+
+test("Basic WikiWord", () => {
+  expect(transformAndRender("WikiWord")).toBe(
+    '<p><a href="WikiWord">WikiWord</a></p>'
+  );
+});
+
+test("Acronyms aren't wikiwords", () => {
+  expect(transformAndRender("ABC")).toBe("<p>ABC</p>");
+});
